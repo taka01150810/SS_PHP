3409769a1ac6ffc133cf2977f1934bfb548e3183
taka01150810 <taka01150810@gmail.com>
2022-02-22 14:49:32 +0900

    その他の演算子(エラー制御演算子)

--- 演算子/other_operator.php
+++ 演算子/other_operator.php
@@ -29,3 +29,14 @@ print '<br/>';
 */
 $result = `ls`;//ls コマンドを実行
 print $result;//結果 compare.php operator.php other_operator.php ref.php shortcut.php
+
+print '<br/>';
+//3.6.3 エラー制御演算子
+/*
+エラー制御演算子（@）は特定の式の先頭に付加することで、その命令で発生したエラーメッセージを抑制します（表示しないようにします）。
+エラー制御演算子（@）はできるだけ使うべきではありません。
+なぜなら、@演算子によってエラーが隠蔽されてしまうので、コードに問題があった場合に原因の特定が困難になるからです。
+*/
+$data = ['apple'=>'りんご'];
+print $data['orange'];//結果：Notice: Undefined index: orange in /Applications/MAMP/htdocs/独習PHP/演算子/other_operator.php on line 36
+print @$data['orange'];//結果：（表示されない）
