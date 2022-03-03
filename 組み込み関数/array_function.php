70b37b0bb901c1db019e3f702e5acf0a97c3393e
taka01150810 <taka01150810@gmail.com>
2022-03-03 12:23:11 +0900

    配列関数(配列の内容を連結する array_merge関数)

--- 組み込み関数/array_function.php
+++ 組み込み関数/array_function.php
@@ -27,3 +27,21 @@ array_count_values関数の戻り値は、「要素値=>登場回数」形式の
 */
 $data=['い','ろ','は','に','ほ','へ','と','い','ろ'];
 print_r(array_count_values($data));//結果 Array ( [い] => 2 [ろ] => 2 [は] => 1 [に] => 1 [ほ] => 1 [へ] => 1 [と] => 1 )
+
+print '<br/>';
+//5.3.2 配列の内容を連結する array_merge関数
+$ary1 = [1,3,5];
+$ary2 = [2,3,6];
+$result = array_merge($ary1, $ary2);
+print_r($result);//結果 Array ( [0] => 1 [1] => 3 [2] => 5 [3] => 2 [4] => 3 [5] => 6 )
+
+print '<br/>';
+$assoc1 = ['Apple' => 'Red', 'Orange' => 'Yellow', 'Melon' => 'Green'];
+$assoc2 = ['Grape' => 'Purple', 'Apple' => 'Green', 'Strawberry' => 'Red'];
+$result = array_merge($assoc1, $assoc2);
+print_r($result);//結果 Array ( [Apple] => Green [Orange] => Yellow [Melon] => Green [Grape] => Purple [Strawberry] => Red )
+
+print '<br/>';
+//結果 配列のキーが重複した場合は（上書きするのではなく）入れ子の配列を生成するarray_merge_recursive関数を使う
+$result = array_merge_recursive($assoc1, $assoc2);
+print_r($result);//結果 Array ( [Apple] => Array ( [0] => Red [1] => Green ) [Orange] => Yellow [Melon] => Green [Grape] => Purple [Strawberry] => Red )
