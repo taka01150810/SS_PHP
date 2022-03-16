9f2c013fed00241cae0e06039de6ed21266fdb1c
taka01150810 <taka01150810@gmail.com>
2022-03-16 12:22:52 +0900

    変数の有効範囲(静的変数 static命令)

--- ユーザー定義関数/variable_scope.php
+++ ユーザー定義関数/variable_scope.php
@@ -31,3 +31,28 @@ print $x;//結果 11
 /*
 これによって、関数内の$xがグローバル変数とみなされます。
 */
+print '<br/>';
+//6.2.3 静的変数 static命令
+/*
+ローカル変数の有効範囲は関数の中だけです。関数の処理が終了するタイミングでローカル変数は破棄されています。
+したがって、下記のような関数はほとんど意味がありません。
+*/
+function checkStatic():int{
+    $x = 0;
+    return ++$x;
+}
+print checkStatic();//結果 1
+print checkStatic();//結果 1
+
+print '<br/>';
+//関数の終了後もローカル変数を維持しておくにはどうすればいいか?
+function checkStatic_2():int{
+    static $x = 0;
+    return ++$x;
+}
+print checkStatic_2();//結果 1
+print checkStatic_2();//結果 2
+/*
+静的変数は、関数の初回呼び出し時にのみ初期化され、その後、関数の処理が終了しても維持されます。
+checkStatic関数を繰り返し呼び出すと、確かに、ローカル変数$xがインクリメントされていることが確認できます。
+*/
