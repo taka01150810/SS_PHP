b593013d8e51cc368c0c12c91ffcf39f432836db
taka01150810 <taka01150810@gmail.com>
2022-03-21 13:28:43 +0900

    関数の呼び出しと戻り値(素数を求めるジェネレーター)

--- ユーザー定義関数/generator.php
+++ ユーザー定義関数/generator.php
@@ -21,3 +21,36 @@ yieldは、returnとよく似た命令で、関数の値を呼び出し元に返
 yield命令は処理を一時停止します(中断された箇所から実行を再開)。
 つまり、次に呼び出されたときには、その時点から処理を再開できます。
 */
+
+//6.5.1 素数を求めるジェネレーター
+//素数を求めるジェネレーター関数
+function getPrimes(){
+    $num = 2;
+    //素数の開始値//2から順に素数判定し、素数の場合にだけyield（無限ループ）
+    while(true){
+        if(isPrime($num)){
+            yield $num;
+        }
+        $num++;
+    }
+}
+//引数$valueが素数かどうかを判定
+function isPrime(int $value):bool{
+    $prime = true;//素数かどうかを表すフラグ
+    //2～sqrt($value)で、$valueを割り切れる（余りが0）のものがあるか
+    for($i = 2;$i <= floor(sqrt($value));$i++){
+        if($value % $i === 0){ 
+            $prime = false;//割り切れるものがあれば素数でない
+            break;
+        }
+    }
+    return $prime;
+}
+//素数を順に出力
+foreach(getPrimes() as $prime){
+    //素数が101以上になったら終了（これがないと無限ループになるので注意！）
+     if($prime > 100){
+         die();
+    }
+    print $prime.',';
+}//結果 2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,
