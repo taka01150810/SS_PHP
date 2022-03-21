34cc151f5254289abf70fbc621bbf89e97d7919d
taka01150810 <taka01150810@gmail.com>
2022-03-21 11:40:17 +0900

    関数の呼び出しと戻り値(再帰関数)

--- ユーザー定義関数/function_return.php
+++ ユーザー定義関数/function_return.php
@@ -13,3 +13,18 @@ $result = max_min(10, -2, -5, 3, 78);
 print_r($result);//結果 Array ( [0] => 78 [1] => -5 )
 [$max, $min] = max_min(10, 2, -5, 3, 78);
 print "最大値:{$max}, 最小値:{$min}";//結果 最大値:78, 最小値:-5
+
+print '<br/>';
+//6.4.2 再帰関数
+/*
+再帰関数（RecursiveFunction）とは、ある関数が自分自身を呼び出すこと、または、そのような関数のことです。
+再帰関数を利用することで、（たとえば）階乗計算のように同種の手続きを繰り返し呼び出すような処理を、短いコードで表現できます。
+階乗とは、自然数nに対する1～nの総積のことです（数学的には「n!」と表記します）
+*/
+function factorial(int $num):int{
+    if($num !== 0){
+        return $num * factorial($num - 1);
+    }
+    return 1;
+ }
+print factorial(5);//結果：120
