5298a78a2fbae683fdccb0736b185531be55bb74
taka01150810 <taka01150810@gmail.com>
2022-04-14 15:52:30 +0900

    DirectoryIteratorクラス

---
+++ 標準クラスライブラリ/directory_iterator.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <mata charset="UTF-8" />
+        <title></title>
+        <link rel="stylesheet" href="https://stachpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstorap.min.css">
+    </head>
+    <body>
+        <table class="table">
+            <thread>
+                <tr>
+                    <th>ファイル</th><th>サイズ</th><th>最終アクセス日</th><th>最終更新日</th>
+                </tr>
+            </thread>
+        </table>
+    </body>
+</html>
+
+<?php
+
+//7.3 DirectoryIteratorクラス
+/*
+DirectoryIteratorクラスは、指定されたフォルダー（ディレクトリ）配下のファイル情報にアクセスするためのクラスです。
+
+フォルダーの読み込みは、大きく次の手順で行います。
+①フォルダーを開く
+②フォルダー配下の要素を順に取得する
+③ファイル情報を取得する
+*/
+?>
